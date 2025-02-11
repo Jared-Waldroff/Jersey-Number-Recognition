3ddf5705ebb6858c645a1b4168fd593f7cb01a0d
Jared-Waldroff <jared.waldroff@gmail.com>
2025-02-11 00:57:34 -0800

    Initial commit of Jersey Number Recognition

---
+++ downloadSoccernet.py
@@ -0,0 +1,6 @@
+from SoccerNet.Downloader import SoccerNetDownloader as SNdl
+
+# Set the local directory where you want to save the dataset
+mySNdl = SNdl(LocalDirectory="./data/SoccerNet")
+# Download train, test, and challenge splits for the jersey number task
+mySNdl.downloadDataTask(task="jersey-2023", split=["challenge"])
