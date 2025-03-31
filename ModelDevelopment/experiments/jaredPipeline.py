33f666e0507f29147f23e2b6bb2071f2a5c6425b
Jared-Waldroff <jared.waldroff@gmail.com>
2025-03-31 09:17:22 -0700

    enhaaced my pipelinne

--- ModelDevelopment/experiments/jaredPipeline.py
+++ ModelDevelopment/experiments/jaredPipeline.py
@@ -12,18 +12,36 @@ from ModelDevelopment.CentralPipeline import CentralPipeline
 from ModelDevelopment.ImageBatchPipeline import ImageBatchPipeline
 from DataProcessing.DataPreProcessing import DataPaths, DataPreProcessing
 
+# Configuration variables
+USE_RANDOM_TRACKLETS = False  # Set to True to use random tracklets, False to process all tracklets
+NUM_RANDOM_TRACKLETS = 4  # Number of random tracklets to process if USE_RANDOM_TRACKLETS is True
+
+# Optional: Specify specific tracklets to process if you want a fixed set
+SPECIFIC_TRACKLETS = []  # Example tracklet IDs
 
 def main():
     # First, get all tracks
     data_preprocessor = DataPreProcessing()
     tracks, max_track = data_preprocessor.get_tracks(DataPaths.TEST_DATA_DIR.value)
 
-    # Randomly select a subset of tracks
-    num_tracklets_to_process = 4  # Change this to the number of random tracklets you want
-    random_tracklets = random.sample(tracks, num_tracklets_to_process)
+    # Determine which tracklets to process
+    if USE_RANDOM_TRACKLETS:
+        # Randomly select a subset of tracks
+        tracklets_to_process = random.sample(tracks, NUM_RANDOM_TRACKLETS)
+        print("Processing the following randomly selected tracklets:")
+        print(tracklets_to_process)
+    elif SPECIFIC_TRACKLETS:
+        # Use predefined specific tracklets
+        tracklets_to_process = SPECIFIC_TRACKLETS
+        print("Processing the following specific tracklets:")
+        print(tracklets_to_process)
+    else:
+        # Process all tracklets
+        tracklets_to_process = tracks
+        print("Processing ALL tracklets")
 
     pipeline = CentralPipeline(
-        tracklets_to_process_override=random_tracklets,  # change to ["34", "666", "1044", "1166"] format for specific tracklets
+        tracklets_to_process_override=tracklets_to_process,  # change to ["34", "666", "1044", "1166"] format for specific tracklets
         # num_tracklets=3,
         # num_images_per_tracklet=50,
         input_data_path=DataPaths.TEST_DATA_DIR.value,
@@ -34,33 +52,29 @@ def main():
         display_transformed_image_sample=False,
         # NOTE: DO NOT USE. Code is parallelized so we cannot show images anymore. Code breaks, but first one will show if True.
         num_image_samples=1,
-        use_cache=True,  # Set to false if you encounter data inconsistencies.
+        use_cache=False,  # Set to false if you encounter data inconsistencies.
         suppress_logging=False,
 
         # --- PARALLELIZATION PARAMS --- These settings are optimal for an NVIDIA RTX 3070 Ti Laptop GPU.
-        num_workers=1,  # CRITICAL optimisation param. Adjust accordingly. 6
-        tracklet_batch_size=32,  # CRITICAL optimisation param. Adjust accordingly. 32
-        image_batch_size=128,  # CRITICAL optimisation param. Adjust accordingly. 200
+        num_workers=2,  # CRITICAL optimisation param. Adjust accordingly. 6
+        tracklet_batch_size=16,  # CRITICAL optimisation param. Adjust accordingly. 32
+        image_batch_size=24,  # CRITICAL optimisation param. Adjust accordingly. 200
         num_threads_multiplier=1,
         # CRITICAL optimisation param. Adjust accordingly. 3. Interpretation: num_threads = num_workers * num_threads_multiplier
     )
 
-    # Print out the randomly selected tracklets
-    print("Processing the following randomly selected tracklets:")
-    print(random_tracklets)
-
     pipeline.run_soccernet(
-      run_soccer_ball_filter=True,
-      generate_features=True,
-      run_filter=True,
-      run_legible=True,
-      run_legible_eval=True,
+      run_soccer_ball_filter=False,
+      generate_features=False,
+      run_filter=False,
+      run_legible=False,
+      run_legible_eval=False,
       run_pose=True,
       run_crops=True,
-      run_str=True,
-      run_combine=True,
-      run_eval=True,
-      use_clip4str=True,
+      run_str=False,
+      run_combine=False,
+      run_eval=False,
+      use_clip4str=False,
       pyscrippt=True)
 
 
